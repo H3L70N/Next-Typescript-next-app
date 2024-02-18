d43a87398a9949731d4fe9235aba37738a6a9483
H3L70N <heltonnguammbe@gmail.com>
2024-02-18 18:35:53 +0200

    Daisyui, Styled With Tailwind

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -3,19 +3,33 @@ import React from "react";
 interface User {
   id: number;
   name: string;
+  email: string;
 }
 
 const UsersPage = async () => {
-  const res: Response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const res: Response = await fetch(
+    "https://jsonplaceholder.typicode.com/users"
+  );
   const users: User[] = await res.json();
   return (
     <>
       <h1>Users</h1>
-      <ul>
-        {users.map((user) => (
-          <li key={user.id}>{user.name}</li>
-        ))}
-      </ul>
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
   );
 };
